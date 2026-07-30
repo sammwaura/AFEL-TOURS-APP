dd08612a1cb0ced5b671bd39dbfa72aa4b42914d
sammwaura <meshsami27@gmail.com>
2026-07-30 15:18:01 +0300

    modify the app file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import {Routes, Route } from 'react-router-dom'
 import Navbar from './components/NavBar'
 import Home from './components/Home'
+import RoomListing from './pages/RoomListing'
+import RoomDetails from './pages/RoomDetails'
 
 
 function App() {
@@ -9,7 +11,8 @@ function App() {
     <div className="min-h-screen bg-paper">
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />}></Route>
+        <Route path='/' element={<Home />} />
+        <Route path='/rooms' element={<RoomListing />} />
       </Routes>
     </div>
   )
